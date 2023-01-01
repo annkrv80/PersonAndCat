0219daae3a9e253633510d57a578f498eb0dc1ea
Anna <annkrv80@yandex.ru>
2023-01-02 01:04:00 +0700

    Класс Кот

---
+++ Cat.java
@@ -0,0 +1,32 @@
+public class Cat {
+    private String name;
+    private String owner;
+    private String voice;
+
+    public Cat(String name, String owner) {
+        this.name = name;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getVoice() {
+        return voice;
+    }
+
+    public void setVoice(String voice) {
+        this.voice = voice;
+    }
+
+    @Override
+    public String toString() {
+
+        return this.name;
+    }
+}
