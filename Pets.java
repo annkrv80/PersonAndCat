764be092e2d7d3d0255004816c9ad136895b1851
Anna <annkrv80@yandex.ru>
2023-01-05 15:14:09 +0700

    Домашние питомцы

---
+++ Pets.java
@@ -0,0 +1,13 @@
+public abstract class Pets {
+    protected String petOwner;
+
+    public String getPetOwner() {
+        return petOwner;
+    }
+
+    public void setPetOwner(String petOwner) {
+        this.petOwner = petOwner;
+    }
+
+    abstract void eat();
+}
