c9153224a099e185b7a79b8fb49f5adb46241543
Anna <annkrv80@yandex.ru>
2023-01-05 15:12:17 +0700

    Кошки

--- Cat.java
+++ Cat.java
@@ -1,32 +1,53 @@
-public class Cat {
-    private String name;
-    private String owner;
-    private String voice;
+public class Cat extends Pets implements PetCallingPerson {
+    private String petName;
+    private String petVoice;
 
-    public Cat(String name, String owner) {
-        this.name = name;
-        this.owner = owner;
+    public Cat(String petName, String petOwner) {
+        this.petName = petName;
+        this.petOwner = petOwner;
     }
 
-    public String getName() {
-        return name;
+    public String getPetName() {
+        return petName;
     }
 
-    public String getOwner() {
-        return owner;
+    public void setPetVoice(String petVoice) {
+        this.petVoice = petVoice;
     }
 
-    public String getVoice() {
-        return voice;
-    }
-
-    public void setVoice(String voice) {
-        this.voice = voice;
+    public String getPetVoice() {
+        return petVoice;
     }
 
     @Override
     public String toString() {
 
-        return this.name;
+        return this.petName;
+    }
+
+    @Override
+    void eat() {
+        System.out.println(this.petName + " пьет молоко!");
+
+    }
+
+    @Override
+    public void callPet(Person p) {
+        System.out.println(p.getName() + " говорит " + p.getVoice());
+        if (p.getVoice() == "kis-kis-kis") {
+            System.out.println(this.petName + " бежит " + p.getName());
+        } else {
+            System.out.println(this.petName + "  делает вид что не понимает!");
+        }
+    }
+
+    @Override
+    public void petCallingPerson(Person p) {
+        if (this.petVoice == "myau-myau") {
+            p.feeding();
+        }
+        if (this.petVoice == "mur-mur-mur") {
+            p.pet();
+        }
     }
 }
